689d22a47aebf03e38ed6f19ad92953a2e61586f
heyZeuss <rubioj@protonmail.com>
2018-10-22 12:27:26 -0400

    Final Push

--- rubioj.buildrooms.c
+++ rubioj.buildrooms.c
@@ -30,45 +30,34 @@
 #define TRUE 1
 #define FALSE 0
 
-/// NAME: boolean
-/// DESC: This helps me not mix up my true and false while programming.
-///       And since we are using an earlier version of C.
-/// SOURCE: http://stackoverflow.com/questions/1909825/error-with-the-declaration-of-enum
 
-//typedef enum {FALSE = 0, TRUE = 1} boolean;
-
-/// NAME: ROOM_NAMES
-/// DESC: Global of possible ROOM names.
-char *ROOM_NAMES[TOTAL_NUM_ROOMS] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "AABBCC" };
-//END_GLOBALS
+char *room_names[TOTAL_NUM_ROOMS] = { "The_Great_Hall", "Room_Of_Requirement", "Library", "Headmasters_Office", 
+                                      "Prefects_Bathroom", "Chamber_Of_Secrets", "Kitchen", 
+                                      "Astronomy_Tower", "Greenhouse", "Trophy_Room" };
+char* room_types[3] = { "START_ROOM", "MID_ROOM", "END_ROOM" };
 
 
-///NAME: ROOM_TYPES
-///DESC: Determines if start,mid, or end ROOM.
-enum ROOM_TYPES{
-    START_ROOM,
-    MID_ROOM,
-    END_ROOM
-};
-
-
-///NAME: ROOM
-///DESC: Defines ROOM specs.
-struct ROOM 
+struct Room 
 {
     int TotalConnections;
-    struct ROOM* Connections[MAX_ROOM_CONNECTIONS];
-    enum ROOM_TYPES RType;
-    char Name[100];
+    struct Room* Connections[MAX_ROOM_CONNECTIONS];
+    char* type;
+    char Name[256];
 };
 
-//ROOM GLOBAL
-struct ROOM ROOMList[MAX_NUM_ROOMS];
-int  ROOMState[TOTAL_NUM_ROOMS];
-char folderName[256];
+struct Room room_list[MAX_NUM_ROOMS];
+int  room_state[TOTAL_NUM_ROOMS];
+char folder_name[256];
 
 
-// Returns TRUE if all rooms have 3 to 6 outbound connections, FALSE otherwise
+/*
+ NAME
+    IsGraphFull
+ SYNPOSIS
+    Takes in nothing and returns an integer
+ DESCRIPTION
+    Returns TRUE if all rooms have 3 to 6 outbound connections, FALSE otherwise
+*/
 int IsGraphFull()  
 {
 
@@ -76,7 +65,7 @@ int IsGraphFull()
   for(i=0; i< MAX_NUM_ROOMS; i++)
   {
   
-    if (!(ROOMList[i].TotalConnections >= MIN_ROOM_CONNECTIONS))
+    if (!(room_list[i].TotalConnections >= MIN_ROOM_CONNECTIONS))
     {
         return FALSE;
     }
@@ -86,8 +75,15 @@ return TRUE;
 }
 
 
-// Returns TRUE if a connection can be added from ROOM x (< 6 outbound connections), FALSE otherwise
-int CanAddConnectionFrom(struct ROOM x) 
+/*
+ NAME
+    CanAddConnectionFrom
+ SYNPOSIS
+    Takes in a struct Room Type and returns an integer
+ DESCRIPTION
+    Returns TRUE if a connection can be added from ROOM x (< 6 outbound connections), FALSE otherwise
+*/
+int CanAddConnectionFrom(struct Room x) 
 {
     if(x.TotalConnections == MAX_ROOM_CONNECTIONS)
     {
@@ -98,8 +94,15 @@ int CanAddConnectionFrom(struct ROOM x)
     }
 }
 
-// Returns a random ROOM, does NOT validate if connection can be added
-struct ROOM GetRandomROOM()
+/*
+ NAME
+    GetRandomRoom
+ SYNPOSIS
+    Takes in nothing and returns a struct Room
+ DESCRIPTION
+    Returns a random ROOM, does NOT validate if connection can be added
+*/
+struct Room GetRandomRoom()
 {
   int p;
 
@@ -107,22 +110,29 @@ struct ROOM GetRandomROOM()
     {
         p = rand() % MAX_NUM_ROOMS + 0; 
 
-    }while(CanAddConnectionFrom(ROOMList[p]) == FALSE && p <= MAX_NUM_ROOMS && p >= 0);
+    }while(CanAddConnectionFrom(room_list[p]) == FALSE && p <= MAX_NUM_ROOMS && p >= 0);
     // check if numbers are out of range.
 
-    return ROOMList[p];
+    return room_list[p];
 }
 
 
-// Returns TRUE if a connection from ROOM x to ROOM y already exists, FALSE otherwise
-int ConnectionAlreadyExists(struct ROOM x, struct ROOM y)
+/*
+ NAME
+    ConnectionAlreadyExists
+ SYNPOSIS
+    Takes in Two struct Rooms and returns an integer
+ DESCRIPTION
+    Returns TRUE if a connection from ROOM x to ROOM y already exists, FALSE otherwise
+*/
+int ConnectionAlreadyExists(struct Room x, struct Room y)
 {
 
   int i;
     //parse through list of connections
     for(i = 0; i < x.TotalConnections;i++)
     {
-        //check to see if the ROOMs are already connected
+        //check to see if the Rooms are already connected
         if(strcmp(x.Connections[i]->Name,y.Name) == 0)
         {
             return TRUE; 
@@ -131,40 +141,54 @@ int ConnectionAlreadyExists(struct ROOM x, struct ROOM y)
     return FALSE;
 }
 
-// Connects ROOMs x and y together, does not check if this connection is valid
-void ConnectROOM(struct ROOM x, struct ROOM y) 
+/*
+ NAME
+    ConnectRoom
+ SYNPOSIS
+    Two struct Rooms are passed in and returns nothing
+ DESCRIPTION
+    Connects ROOMs x and y together, does not check if this connection is valid
+*/
+void ConnectRoom(struct Room x, struct Room y) 
 {
  
   int posx, posy,i;
 
   for(i=0; i<MAX_NUM_ROOMS; i++)
   {
-    if(strcmp(ROOMList[i].Name,x.Name) == 0)
+    if(strcmp(room_list[i].Name,x.Name) == 0)
     {
         posx=i;
       
     }
   }
-printf("x.Name: %s is at %d in ROOMList\n", x.Name, posx);
 
   for(i=0; i<MAX_NUM_ROOMS; i++)
   {
-    if(strcmp(ROOMList[i].Name,y.Name) == 0)
+    if(strcmp(room_list[i].Name,y.Name) == 0)
     {
         posy=i;
        
     }
   }
     //connect ROOMs and increment their connection counts
-    ROOMList[posx].Connections[x.TotalConnections] = &ROOMList[posy];
-    ROOMList[posx].TotalConnections++;
+    room_list[posx].Connections[x.TotalConnections] = &room_list[posy];
+    room_list[posx].TotalConnections++;
 
 }
 
-// Returns TRUE if ROOMs x and y are the same ROOM, FALSE otherwise
-int IsSameROOM(struct ROOM x, struct ROOM y) 
+/*
+ NAME
+    IsSameRoom
+ SYNPOSIS
+    Two struct Rooms are passed in and in Int is returned 
+ DESCRIPTION
+    Compares the names of the two rooms passed in
+    Returns TRUE if ROOMs x and y are the same ROOM, FALSE otherwise
+*/
+int IsSameRoom(struct Room x, struct Room y) 
 
-{   //compare the names of the two ROOMs to make sure they are not the same
+{   
 
    if(strcmp(x.Name, y.Name)== 0)
   {
@@ -175,16 +199,21 @@ int IsSameROOM(struct ROOM x, struct ROOM y)
   }
 } 
 
-// Adds a random, valid outbound connection from a ROOM to another ROOM
+/*
+ NAME
+   AddRandomConnection
+ DESCRIPTION
+   Adds a random, valid outbound connection from a ROOM to another ROOM
+*/ 
 void AddRandomConnection()  
 {
-  struct ROOM A;  // Maybe a struct, maybe global arrays of ints
-  struct ROOM B;
+  struct Room A;  // Maybe a struct, maybe global arrays of ints
+  struct Room B;
 
   
   while(TRUE)
   {
-    A = GetRandomROOM();
+    A = GetRandomRoom();
 
     if (CanAddConnectionFrom(A) == TRUE)
       break;
@@ -192,166 +221,135 @@ void AddRandomConnection()
 
   do
   {
-    B = GetRandomROOM();
+    B = GetRandomRoom();
   }
-  while(CanAddConnectionFrom(B) == FALSE || IsSameROOM(A, B) == TRUE || ConnectionAlreadyExists(A, B) == TRUE);
+  while(CanAddConnectionFrom(B) == FALSE || IsSameRoom(A, B) == TRUE || ConnectionAlreadyExists(A, B) == TRUE);
 
-  ConnectROOM(A, B);  // TODO: Add this connection to the real variables, 
-  ConnectROOM(B, A);  //  because this A and B will be destroyed when this function terminates
+  ConnectRoom(A, B);  // TODO: Add this connection to the real variables, 
+  ConnectRoom(B, A);  //  because this A and B will be destroyed when this function terminates
 }
 
 
 
-/// NAME: InitROOMList
-/// DESC: gens a the ROOMlist global with default values
-void InitROOMList()
-{
-   
-}
 
-/// NAME: FillROOMList
-/// DESC: Begins generating the graph.
-void FillROOMList()
+
+/*
+ NAME
+   MakeMyRooms
+ DESCRIPTION
+   Will select 7 rooms - set the total connections to null and then randomly select from the list of 10
+   to connect.
+*/
+void MakeMyRooms()
 {
      int i;   
-    // tells me which 7 ROOMs I picked out of 10
+    
+    //room_state used to control which rooms will be used and connected
     for(i = 0; i < MAX_NUM_ROOMS;i++){
-        ROOMState[i] = FALSE;
+        room_state[i] = FALSE;
     }
 
    
     for(i = 0; i < MAX_NUM_ROOMS;i++){
-        ROOMList[i].TotalConnections = 0;
+        room_list[i].TotalConnections = 0;
 
-        //set all ROOM connections to NULL
+        //set all Room connections to NULL
         int j;
         for(j = 0; j < MAX_ROOM_CONNECTIONS; j++)
         {
-            ROOMList[i].Connections[j] = NULL;
+            room_list[i].Connections[j] = NULL;
         }
 
         while(TRUE){
-            int RandomROOM = rand() % TOTAL_NUM_ROOMS; // sets a ROOM name to be used.
-            if(ROOMState[RandomROOM] == FALSE)
+            int random_room = rand() % TOTAL_NUM_ROOMS; 
+          
+            if(room_state[random_room] == FALSE)
             {  
-                // change ROOM state to TRUE so as not to cbe chosen again
-                ROOMState[RandomROOM] = TRUE;
+                // change room state to TRUE so as not to cbe chosen again
+                room_state[random_room] = TRUE;
 
                 //remove garbage data from ROOM arrray
-                memset(ROOMList[i].Name,'\0',sizeof(ROOMList[i].Name));
-                strcpy(ROOMList[i].Name,ROOM_NAMES[RandomROOM]);
+                memset(room_list[i].Name,'\0',sizeof(room_list[i].Name));
+                strcpy(room_list[i].Name,room_names[random_room]);
 
-                ROOMList[i].RType = MID_ROOM;
+                room_list[i].type = room_types[1];
                 break;
             }
         }
     }
-    //defines a start and winning ROOM.
-    ROOMList[0].RType = START_ROOM;
-    ROOMList[MAX_NUM_ROOMS - 1].RType = END_ROOM;
+
+    //since rooms are randomly chosen assigning first room to be START ROOM 
+    //and last room in array to be END ROOM
+    room_list[0].type = room_types[0];
+    room_list[MAX_NUM_ROOMS - 1].type = room_types[2];
 
 
     while(IsGraphFull() == FALSE)
-     {
-
-
-                AddRandomConnection();
+    {
+      AddRandomConnection();
     } 
 }
 
 
-/// NAME: PrintROOMs_DEBUG
-/// DESC: DEBUG FUNCTION lets me check if a ROOMlist is correct.
-void PrintROOMs_DEBUG()
-{
-    int i,j;
-    for(i = 0; i < MAX_NUM_ROOMS;i++){
-        printf("\n%d: ",i);
-        printf("Name: %s",ROOMList[i].Name);
-        printf("\nTotalConnections: %d",ROOMList[i].TotalConnections);
-        if(ROOMList[i].TotalConnections > 0){
-            printf("\n\tConnections:");
-            for(j = 0;j < ROOMList[i].TotalConnections;j++){
-                printf("\n\tC%d:%s",j,ROOMList[i].Connections[j]->Name);
-                printf("\n\tC%d connectTotal: %d",j,ROOMList[i].Connections[j]->TotalConnections);
-            }
-        }
-        
-        if(ROOMList[i].RType == START_ROOM){
-            printf("\nROOM Type: START_ROOM");
-        }
-        else if(ROOMList[i].RType == END_ROOM){
-            printf("\nROOM Type: END_ROOM");
-        }
-        else{
-            printf("\nROOM Type: MID_ROOM");
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
 
-/// NAME: GenROOMDir
-/// DESC: creates a new ROOMs directory with moderately secure permissions.
-void GenROOMDir()
+/*
+ NAME
+   MakeFileDirectory
+ DESCRIPTION
+   Makes a file directory using the current pid and sets the permissions
+*/
+void MakeFileDirectory()
 {
-    char* staticDirName = "rubioj.rooms.";
+    char* directory_name = "rubioj.rooms.";
     int pid = getpid();
-    int ModeratlyecPermissionSetting = 0770;// rwxrwx---
-    int unsecPermissionSetting = 0777; // rwxrwxrwx EXTREMELY UNSAFE
 
-    memset(folderName,'\0',sizeof(folderName));
-    sprintf(folderName,"%s%d",staticDirName,pid); // sets the new folder name.
+    //clear out array for folder name of any garbage data
+    memset(folder_name,'\0',sizeof(folder_name));
+    //set folder name to as specs require
+    sprintf(folder_name,"%s%d",directory_name,pid);
 
-    //printf("%s",folderName);
-    mkdir(folderName,ModeratlyecPermissionSetting); // creates folder.
+    //set the permissions to read and write  
+    mkdir(folder_name,0770); 
 }
 
 
-/// NAME: GenROOMFiles
-/// DESC: copys ROOMlist struct and prints to a file.
-void GenROOMFiles()
+
+/*
+ NAME
+   MakeMyFile
+ DESCRIPTION
+   Calls MakeFileDirectory and saves rooms files in the made directory
+*/
+void MakeMyFiles()
 {
-    FILE *ROOMFile;
+    FILE *Room_File;
     int i,j;
-    char folderDIR[256];
+    char folder_directory[256];
 
-    sprintf(folderDIR,"./rubioj.rooms.%d",getpid()); // gets the folders future directory.
+    sprintf(folder_directory,"./rubioj.rooms.%d",getpid()); 
 
-    GenROOMDir(); // sets a folder.
+    MakeFileDirectory(); // sets a folder.
     
-    // prevents errors, checks if the folder exists or directory was changed.
-    if(chdir(folderDIR) != 0){
-        printf("DIR NOT CHANGED TO: %s\n",folderDIR);
+    //check to see if folder already exists and prevents error if it does
+    if(chdir(folder_directory) != 0){
+        printf("Directory has been changed: %s\n",folder_directory);
         return;
     }
 
-    //begins genning files
-    for(i = 0; i < MAX_NUM_ROOMS; i++){
-        //no conflicts should happen since the directory was 
-        //dynamically generated.
-        ROOMFile = fopen(ROOMList[i].Name,"w");
+   //parse through the room_list and make a file for each
+    for(i = 0; i < MAX_NUM_ROOMS; i++)
+    {
+       Room_File = fopen(room_list[i].Name,"w");
         
-        //prints ROOM name then connections
-        fprintf(ROOMFile,"ROOM NAME: %s\n",ROOMList[i].Name);
-        for(j = 0;j < ROOMList[i].TotalConnections;j++){
-            fprintf(ROOMFile,"CONNECTION %d: %s\n",j+1,ROOMList[i].Connections[j]->Name);
+        fprintf(Room_File,"ROOM NAME: %s\n",room_list[i].Name);
+        //pare through each room struct connection to print to file
+        for(j = 0;j < room_list[i].TotalConnections;j++)
+        {
+            fprintf(Room_File,"CONNECTION %d: %s\n",j+1,room_list[i].Connections[j]->Name);
         }
         
-        //checks the ROOM type and prints to file.
-        if(ROOMList[i].RType == START_ROOM){
-            fprintf(ROOMFile,"ROOM TYPE: %s\n","START_ROOM");
-        }
-        else if(ROOMList[i].RType == MID_ROOM){
-            fprintf(ROOMFile,"ROOM TYPE: %s\n","MID_ROOM");
-        }
-        else if(ROOMList[i].RType == END_ROOM){
-            fprintf(ROOMFile,"ROOM TYPE: %s\n","END_ROOM");
-        }
-        else{
-            fprintf(ROOMFile,"ROOM TYPE: %s\n","NULL");
-        }
-        fclose(ROOMFile);
+        fprintf(Room_File,"ROOM TYPE: %s\n",room_list[i].type);
+        fclose(Room_File);
     }
 }
 
@@ -359,8 +357,7 @@ void GenROOMFiles()
 int main(void)
 {
     srand(time(NULL));
-    FillROOMList();
-  //  PrintROOMs_DEBUG();
-    GenROOMFiles();
+    MakeMyRooms();
+    MakeMyFiles();
     return 0;
 }
